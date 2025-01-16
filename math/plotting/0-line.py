21a6bfae938167aa1822d06297d2ac722194cb2f
IrakizaGaius <g.irakiza@alustudent.com>
2025-01-16 11:32:24 +0200

    Defining boundaries for better Grapgh Clarity

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,12 +2,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+x0 = 0
+xf = 10
 
-y = np.arange(0, 11) ** 3
-# Plotting the graph
-plt.plot(range(0, 11), y, 'r-', label='y = x^3')
+x = np.arange(x0, xf + 1)
+y = x ** 3
+
+plt.plot(x, y, 'r-', label='y = x^3')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Line Graph of y = x^3')
+plt.xlim([x.min(), x.max()])
+plt.ylim([y.min(), y.max()])
 plt.legend()
 plt.show()
