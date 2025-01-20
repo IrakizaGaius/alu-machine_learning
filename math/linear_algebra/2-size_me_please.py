3af04ad4b999e3dda7f774b4aa40240c92aa61d7
IrakizaGaius <g.irakiza@alustudent.com>
2025-01-20 14:57:28 +0200

    Module Level doc

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+"""
+Module: 2-size_me_please
+This module contains functions for matrix operations, including
+calculating the shape (dimensions) of a matrix.
+Functions:
+    matrix_shape(matrix): Returns the shape of the matrix, represented
+    as a list of integers indicating the number of rows, columns, etc.
+"""
+
+
 def matrix_shape(matrix):
     """
     Calculates the shape of a matrix.
