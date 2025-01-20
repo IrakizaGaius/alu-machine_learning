d9b560dc4ad30d762be0198a13215bb789600320
IrakizaGaius <g.irakiza@alustudent.com>
2025-01-20 14:39:50 +0200

    Matrix trimming and sizing

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix.
+    
+    Args:
+        matrix: A list (or nested lists) representing a matrix.
+    
+    Returns:
+        A list of integers representing the shape of the matrix.
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
