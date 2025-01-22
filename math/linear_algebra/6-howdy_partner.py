06f81f998d8563a047f9d0c1788938a625336d82
IrakizaGaius <g.irakiza@alustudent.com>
2025-01-22 15:40:27 +0200

    Arrray concatenation

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module: 6-howdy_partner
+This module contains functions for matrix operations, including
+matrix addition.
+Functions:
+    cat_arrays(arr1, arr2): Returns the concatenation of two arrays.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays.
+    Args:
+        arr1: A list representing an array.
+        arr2: A list representing an array.
+    Returns:
+        A new list representing the concatenation of the arrays.
+    """
+    return arr1 + arr2
