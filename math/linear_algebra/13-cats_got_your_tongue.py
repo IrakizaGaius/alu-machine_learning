d456e41526c5820a4a0dacafe48966b58db138c5
IrakizaGaius <g.irakiza@alustudent.com>
2025-01-24 12:12:33 +0200

    Add numpy import to matrix concatenation and multiplication files

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -4,6 +4,8 @@
 
 
 import numpy as np
+
+
 def np_cat(mat1, mat2, axis=0):
     """ np_cat - concatenates two numpy.ndarrays """
     return __import__('numpy').concatenate((mat1, mat2), axis)
--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -2,6 +2,8 @@
 
 """ 14-saddle_up  """
 
+import numpy as np
+
 
 def np_matmul(mat1, mat2):
     """ np_matmul - performs matrix multiplication on numpy.ndarrays """
