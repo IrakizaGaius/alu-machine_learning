72fd62946141a0695cc94e59844c361be7f9ae45
IrakizaGaius <g.irakiza@alustudent.com>
2025-01-24 12:09:40 +0200

    Add numpy import for concatenation function

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -3,6 +3,7 @@
 """ 13-cats_got_your_tongue  """
 
 
+import numpy as np
 def np_cat(mat1, mat2, axis=0):
     """ np_cat - concatenates two numpy.ndarrays """
     return __import__('numpy').concatenate((mat1, mat2), axis)
