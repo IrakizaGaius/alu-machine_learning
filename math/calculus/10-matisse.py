962afe186cbabd1026d198e07edef41b28381ba2
IrakizaGaius <g.irakiza@alustudent.com>
2025-02-11 15:35:25 +0200

    Add polynomial derivative and summation of squares functions

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Derivative of a polynomial"""
+
+def poly_derivative(poly: list) -> list:
+    """Derivative of a polynomial"""
+    if not isinstance(poly, list) or not all(isinstance(i, (int, float)) for i in poly):
+        return None
+    return [poly[i] * i for i in range(1, len(poly))] if len(poly) > 1 else [0]
