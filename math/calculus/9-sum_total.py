f044bceaaa5e765f731141cbfc8f28f7d8e90dec
IrakizaGaius <g.irakiza@alustudent.com>
2025-02-11 15:35:25 +0200

    Add polynomial derivative and summation of squares functions

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Sum of the squares of the first n natural numbers"""
+
+def summation_i_squared(n: int) -> int:
+    """Sum of the squares of the first n natural numbers"""
+    if not isinstance(n, int) or n < 1:
+        return None
+    return n * (n + 1) * (2 * n + 1) // 6
