25c51bd50367364897b721b4f573d0c35fe00d90
IrakizaGaius <g.irakiza@alustudent.com>
2025-02-19 16:54:03 +0200

    Add main script for Poisson distribution and implement PMF method

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -22,3 +22,17 @@ class Poisson:
             raise ValueError("data must contain multiple values")
         else:
             self.lambtha = sum(data) / len(data)
+
+    def pmf(self, k):
+        """
+        Probability Mass Function
+        """
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+        return ((2.7182818285 ** (-self.lambtha)) *
+                (self.lambtha ** k)) / factorial
