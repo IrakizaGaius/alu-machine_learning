86a117ab031f4ad6c87a7b28fc1d5db6edc60a61
IrakizaGaius <g.irakiza@alustudent.com>
2025-05-18 13:09:34 +0200

    Implement Neuron class with forward propagation and cost calculation for binary classification

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""Defines a single neuron performing binary classification"""
+import numpy as np
+
+
+class Neuron:
+    """Class Neuron that defines a single neuron
+performing binary classification"""
+    def __init__(self, nx):
+        """Constructor of the class Neuron
+        Args:
+            nx (int): number of input features to the neuron
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx <= 0:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for A"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron
+        Args:
+            X (numpy.ndarray): input data of shape (nx, m)
+                nx: number of input features
+                m: number of examples
+        Returns:
+            A (numpy.ndarray): activated output of the neuron
+        """
+        z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression
+        Args:
+            Y (numpy.ndarray): correct labels for the input data
+                shape (1, m)
+            A (numpy.ndarray): activated output of the neuron
+                shape (1, m)
+        Returns:
+            cost (float): cost of the model
+        """
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A)) / m
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neuron’s predictions
+        Args:
+            X (numpy.ndarray): input data of shape (nx, m)
+                nx: number of input features
+                m: number of examples
+            Y (numpy.ndarray): correct labels for the input data
+                shape (1, m)
+        Returns:
+            A (numpy.ndarray): activated output of the neuron
+            cost (float): cost of the network
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        return A, cost
