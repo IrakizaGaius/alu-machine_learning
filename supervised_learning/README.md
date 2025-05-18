6259742c7df3b53d1f2816d8d7c6b23de1a34ec1
IrakizaGaius <g.irakiza@alustudent.com>
2025-05-18 12:42:35 +0200

    initialization of the project and

---
+++ supervised_learning/README.md
@@ -0,0 +1,3 @@
+# alu-machine_learning-supervide_learning
+
+This Repository Introduces different aspects of supervised learning of a model in machine Learning
---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+class Neuron:
+    """Class Neuron that defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        """Constructor of the class Neuron
+        Args:
+            nx (int): number of input features to the neuron
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx <= 0:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
