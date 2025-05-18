193edc0f777c50e9b460113992c310950b5d11e9
IrakizaGaius <g.irakiza@alustudent.com>
2025-05-18 15:53:28 +0200

    Validate parameters in train method for iterations and alpha

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -112,6 +112,15 @@ performing binary classification"""
             A (numpy.ndarray): activated output of the neuron
             cost (float): cost of the network
         """
+        if not isinstance(iterations, int):
+            raise TypeError('iterations must be an integer')
+        if iterations < 0:
+            raise ValueError('iterations must be a positive integer')
+        if not isinstance(alpha, float):
+            raise TypeError('alpha must be a float')
+        if alpha < 0:
+            raise ValueError('alpha must be positive')
+
         for i in range(iterations):
             A = self.forward_prop(X)
             self.gradient_descent(X, Y, A, alpha)
