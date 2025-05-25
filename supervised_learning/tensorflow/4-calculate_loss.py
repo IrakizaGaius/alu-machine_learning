81373d6b65dd365774c7bd39843f33d4b45ad30b
IrakizaGaius <g.irakiza@alustudent.com>
2025-05-25 16:27:23 +0200

    Refactor calculate_loss function to use tf.nn.softmax_cross_entropy_with_logits_v2 for improved accuracy

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -16,4 +16,7 @@ def calculate_loss(y, y_pred):
         tf.Tensor: tensor containing the loss of the prediction
     """
 
-    return tf.compat.v1.losses.softmax_cross_entropy(y, y_pred)
+    loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits_v2(
+            labels=y, logits=y_pred))
+    return loss
