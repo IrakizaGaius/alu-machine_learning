65da60d1bf98e03c330472c8961e6ecb299598a6
IrakizaGaius <g.irakiza@alustudent.com>
2025-05-25 16:29:38 +0200

    Add create_train_op function for Adam optimization training operation

---
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Train operation for a neural network in TensorFlow."""
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+    """
+    Creates a training operation using Adam optimization.
+
+    Args:
+        loss (tf.Tensor): The loss tensor to minimize.
+        alpha (float): The learning rate.
+
+    Returns:
+        tf.Operation: The training operation.
+    """
+    optimizer = tf.train.AdamOptimizer(learning_rate=alpha)
+    train_op = optimizer.minimize(loss)
+    return train_op
