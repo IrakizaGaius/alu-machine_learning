cb70d87559e5743a24dafff33249f61bfbb1db96
IrakizaGaius <g.irakiza@alustudent.com>
2025-06-24 16:22:29 +0200

    Refactor upcoming launch script for clarity and error handling

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -1,62 +1,46 @@
 #!/usr/bin/env python3
-"""Script that prints the upcoming launch in the correct format."""
+"""Script that prints the upcoming SpaceX launch."""
 
 import requests
-from datetime import datetime, timezone
+from datetime import datetime
 
-if __name__ == '__main__':
-    """Fetches the next SpaceX launch and
-    prints its details in a specific format."""
+def get_upcoming_launch():
     launches_url = "https://api.spacexdata.com/v4/launches/upcoming"
     rockets_url = "https://api.spacexdata.com/v4/rockets/"
     launchpads_url = "https://api.spacexdata.com/v4/launchpads/"
 
-    try:
-        response = requests.get(launches_url)
-        if response.status_code == 404:
-            print("Not found")
-        elif response.status_code == 403:
-            print("Access forbidden")
-        elif response.status_code == 200:
-            data = response.json()
+    response = requests.get(launches_url)
+    if response.status_code != 200:
+        print("Error fetching launches")
+        return
 
-            # Sort by date_unix
-            data.sort(key=lambda x: x.get("date_unix", float('inf')))
-            next_launch = data[0]
+    data = response.json()
+    data.sort(key=lambda x: x.get("date_unix", float("inf")))
+    next_launch = data[0]
 
-            # Get launch details
-            launch_name = next_launch.get("name", "N/A")
-            d_utc = next_launch.get("date_utc", "")
-            # Formatted Date
-            f_date = (
-                datetime.strptime(d_utc, "%Y-%m-%dT%H:%M:%S.%fZ")
-                .strftime("%Y-%m-%d %H:%M:%S")
-            )
-            # Get rocket name
-            rocket_id = next_launch.get("rocket")
-            rocket_name = "N/A"
-            if rocket_id:
-                rocket_res = requests.get(rockets_url + rocket_id)
-                if rocket_res.status_code == 200:
-                    rocket_name = rocket_res.json().get("name", "N/A")
+    launch_name = next_launch.get("name", "N/A")
+    date_utc = next_launch.get("date_utc", "")
+    local_dt = datetime.strptime(date_utc, "%Y-%m-%dT%H:%M:%S.%fZ")
+    local_str = local_dt.isoformat()
 
-            # Get launchpad name and locality
-            launchpad_id = next_launch.get("launchpad")
-            launchpad_name = "N/A"
-            locality = "N/A"
-            if launchpad_id:
-                pad_res = requests.get(launchpads_url + launchpad_id)
-                if pad_res.status_code == 200:
-                    pad_data = pad_res.json()
-                    launchpad_name = pad_data.get("name", "N/A")
-                    locality = pad_data.get("locality", "N/A")
+    rocket_id = next_launch.get("rocket")
+    rocket_name = "N/A"
+    if rocket_id:
+        rocket_res = requests.get(rockets_url + rocket_id)
+        if rocket_res.status_code == 200:
+            rocket_name = rocket_res.json().get("name", "N/A")
 
-            print(
-                f"{launch_name} ({f_date}) {rocket_name} - "
-                f"{launchpad_name} ({locality})"
-            )
+    launchpad_id = next_launch.get("launchpad")
+    launchpad_name = "N/A"
+    locality = "N/A"
+    if launchpad_id:
+        pad_res = requests.get(launchpads_url + launchpad_id)
+        if pad_res.status_code == 200:
+            pad_data = pad_res.json()
+            launchpad_name = pad_data.get("name", "N/A")
+            locality = pad_data.get("locality", "N/A")
 
-        else:
-            print("Unexpected error: {}".format(response.status_code))
-    except requests.RequestException as e:
-        print("Error: {}".format(e))
+    print(f"{launch_name} ({local_str}) {rocket_name} - {launchpad_name} ({locality})")
+
+if __name__ == '__main__':
+    get_upcoming_launch()
